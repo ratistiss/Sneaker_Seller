a7efa6cfe20f88f82cde25c88f66469c9b85efcc
ratistiss <50683985+ratistiss@users.noreply.github.com>
2020-06-26 17:36:40 -0400

    working on schema

--- database/schema.py
+++ database/schema.py
@@ -1,1 +1,27 @@
-schema.py
+import sqlite3
+import os
+
+PATH = os.path.dirname(__file__)
+DATAPATH = os.path.join(PATH, "sneakers.db")
+print(DATAPATH)
+
+def Schema():
+    with sqlite3.connect(DATAPATH) with conn:
+        c = conn.cursor()
+        table = """CREATE TABLE IF NOT EXISTS sneaker (
+                inv_key INTEGER PRIMARY KEY AUTOINCREMENT,
+                sneaker_name VARCHAR(24),
+                year_released INTEGER,
+                version_num INTERGER,
+                creator VARCHAR(24),
+                orig_price FLOAT,
+                curr_price FLOAT,
+                manufacturer VARCHAR(16),
+                phone VARCHAR(24),
+                email VARCHAR
+            )"""
+        c.execute(table)
+
+
+if __name__ == "__main__":
+    Schema()
--- run.py
+++ run.py
@@ -1,1 +1,11 @@
-run.py
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World’
+
+if __name__ == '__main__':
+   app.run(debug=True)
