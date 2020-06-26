24f2f5f606290f68070baf610c629e18805a47cd
ratistiss <50683985+ratistiss@users.noreply.github.com>
2020-06-26 18:33:28 -0400

    inheritance parent child

--- models/list_customers.py
+++ models/list_customers.py
@@ -1,1 +1,10 @@
 import sqlite3
+
+
+class Customers(Listings):
+
+    def __init__(self, full_name, phone, email):       
+        Listings.__init__(self,inv_key,name, year_released,version_num,creator,orig_price,curr_price,manufacturer)
+            self.name = name
+            self.phone = phone
+            self.email = email
--- models/list_sneakers.py
+++ models/list_sneakers.py
@@ -1,1 +1,20 @@
 import sqlite3
+
+class Listings:
+    table = sneaker
+    dbpath = ""
+
+
+    def __init__(self, inv_key,name, year_released,version_num,creator,orig_price,curr_price,manufacturer):
+        self.inv_key = inv_key
+        self.name = name
+        self.year_released = year_released
+        self.version_num = version_num
+        self.creator = creator
+        self.orig_price = orig_price
+        self.curr_price = curr_price
+        self.manufacturer = manufacturer
+
+    def
+
+    
