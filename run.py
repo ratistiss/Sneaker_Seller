f674103b6150f6b134321fae154cc4d3fe445008
ratistiss <50683985+ratistiss@users.noreply.github.com>
2020-06-28 17:03:24 -0400

    removed a debug directory

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from routes.route import app
 from models.list_sneakers import Listings
 
-Listings.dbpath = "/Users/manderson/Documents/Sneaker_Seller/Sneaker_Seller/database/sneakers.db"
+Listings.dbpath = ""
 
 app.run(debug=True)
